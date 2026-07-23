ec361a774fd07cf1a4f35b4a7a95a310109b6b08
Guruprasad Tukaram Shinde <guruprasadshinde80@gmail.com>
2026-07-23 18:26:04 +0000

    Variables declaration rules

---
+++ Basics/variables.js
@@ -0,0 +1,14 @@
+console.log("Program To Demonstrate Declaration of Variables:\n");
+
+const PI=3.142  //Declare Constant Variable that is immutble
+
+let name="Guru" // prefered to use on var
+
+var surname="Shinde"
+var surname="Mane"  // prefere not to use
+
+middlename="Tukaram"    // not prefered
+
+let clg;    //default value is Undefined
+
+console.table([PI,name,surname,middlename,clg])
